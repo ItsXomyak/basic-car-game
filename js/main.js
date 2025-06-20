ca39294c1ccb9a77f4013ce279e25bc2fe3f2343
scrameee <itsxomyak@gmail.com>
2025-06-20 13:47:02 +0500

    fix: optimize func for get coords

--- js/main.js
+++ js/main.js
@@ -6,25 +6,30 @@
     const trees = document.querySelectorAll('.tree');
     const speed = 3
     const tree1 = trees[0]
+    const coordsTree1 = getCoords(tree1);
 
-    animationId
+    const coords = getCoords(tree1);
+
+    animationId = requestAnimationFrame(startGame);
     function startGame(){
 
     treesAnimation()
 
      animationId = requestAnimationFrame(startGame);
     }
-
+    
     function treesAnimation() {
-      const newCoord = getYCoord(tree1) + speed;
-      tree1.style.transform = `translateY(${newCoord}px)`
+      const newCoordY = coordsTree1.y + speed;
+      coordsTree1.y = newCoordY;
+      tree1.style.transform = `translate(${coordsTree1.x}px, ${coordsTree1.y}px)`;
     }
 
-    function getYCoord(element) {
+    function getCoords(element) {
       const matrix = window.getComputedStyle(tree1).transform
 			const array = matrix.split(',')
+      let getXCoord = parseFloat(array[array.length - 2])
 			let getYCoord = parseFloat(array[array.length - 1])
-      return getYCoord
+      return { x: getXCoord, y: getYCoord };
     }
 
     const gameButton = document.querySelector('.game-button');
@@ -35,6 +40,7 @@
         gameButton.children[0].style.display = 'none';
         gameButton.children[1].style.display = 'initial';
       } else {
+        animationId = requestAnimationFrame(startGame);
         gameButton.children[0].style.display = 'initial';
         gameButton.children[1].style.display = 'none';
       }
